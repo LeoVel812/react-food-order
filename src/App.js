bfc7eae1b3cd9d3f2ddedb757e4adf6fee2a6a65
LeoVel812 <leovelazquez812@gmail.com>
2023-04-26 20:42:34 -0600

    add car visibility state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
+import { useState } from 'react';
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import CartProvider from './store/CartProvider';
 
 function App() {
+  const [cartIsShown,setCartIsShwon]=useState(false);
+  
+  function showCartHandler(){
+    setCartIsShwon(true);
+  }
+
+  function hideCartHandler(){
+    setCartIsShwon(false);
+  }
+
+
   return (
     <CartProvider>
-      <Header />
+      {cartIsShown&&<div>Cart...</div>}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
